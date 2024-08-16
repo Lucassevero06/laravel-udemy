336926af0c7d7694aa82181ea2e10a60288e7728
lucas.severo <lucasgabriel31032006@gmail.com>
2024-08-16 08:55:48 -0400

    estudando laravel

--- app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use function var_dump;
 
 class ContatoController extends Controller
 {
     public function contato()
     {
-        return view('site.contato');
+        return view('site.contato', ['titulo' => 'Contato']);
     }
 }
