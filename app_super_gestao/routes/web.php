0d372f1cfc911b9dcbdc12bee840961669c9ee6e
lucas.severo <lucasgabriel31032006@gmail.com>
2024-08-21 10:02:59 -0400

    estudando laravel

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\LogAcessoMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,14 +18,12 @@ Route::get('/', function () {
 });
 */
 
-Route::middleware(LogAcessoMiddleware::class)
-    ->get('/', 'PrincipalController@principal')
+Route::get('/', 'PrincipalController@principal')
     ->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')
     ->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')
-    ->name('site.contato')
-    ->middleware(LogAcessoMiddleware::class);
+    ->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')
     ->name('site.contato');
 Route::get('/login', function(){return 'Login';})
